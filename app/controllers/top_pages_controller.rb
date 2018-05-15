447e00b4b79ae387623b3d8d18c62355eda99f62
川上雄大 <yudainepnep@icloud.com>
2018-05-15 18:00:00 +0900

    ファイルのアップロードと表示の実装

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,15 +1,12 @@
 class TopPagesController < ApplicationController
 	def index
-		
 	end
 
 	def create
-		uploaded_file = fileupload_params[:file]
-		output_path = Rails.root.join('public', uploaded_file.original_filename)
-		if uploaded_file
-			File.open(output_path, 'w+b') do |fp|
-				fp.write  uploaded_file.read
-			end
+		if params[:image]
+			image_name = "target_img.jpg"
+			image = params[:image]
+			File.binwrite("public/user_images/#{image_name}", image.read)
 		end
 
 		redirect_to(root_url)
