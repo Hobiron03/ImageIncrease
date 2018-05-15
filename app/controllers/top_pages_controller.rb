d728ec6a09878df405fc10665601026d1c6bea4e
川上雄大 <yudainepnep@icloud.com>
2018-05-15 16:50:17 +0900

    Update Gemfile for Heroku

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -3,5 +3,20 @@ class TopPagesController < ApplicationController
 		
 	end
 
-	
+	def create
+		uploaded_file = fileupload_params[:file]
+		output_path = Rails.root.join('public', uploaded_file.original_filename)
+		if uploaded_file
+			File.open(output_path, 'w+b') do |fp|
+				fp.write  uploaded_file.read
+			end
+		end
+
+		redirect_to(root_url)
+	end
+
+	private
+	def fileupload_params
+		params.require(:fileupload).permit(:file)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top_pages#index"
 
-  post '/fileuploads' => "top_pages#fileupload"
+  post '/fileuploads' => "top_pages#create"
 end
