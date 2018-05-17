e2d5591c37ec27e8ff0cbcd700ba5329598cdf33
川上雄大 <yudainepnep@icloud.com>
2018-05-17 23:13:45 +0900

    画像のアップロードと取り消しをできるようにした

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -2,12 +2,20 @@ class TopPagesController < ApplicationController
 	def index
 	end
 
+	def delete_image
+		if File.exist?("public/user_images/target_img.jpg")
+			File.delete ("public/user_images/target_img.jpg")
+		end
+		redirect_to(root_url)
+	end
+
 	def create
 		if params[:image]
 			image_name = "target_img.jpg"
 			image = params[:image]
 			File.binwrite("public/user_images/#{image_name}", image.read)
 		end
+		system('python3 increase_image.py public/user_images/target_img.jpg')
 
 		redirect_to(root_url)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "top_pages#index"
-
+  get "/reset" => "top_pages#delete_image"
   post '/upload' => "top_pages#create"
 end
